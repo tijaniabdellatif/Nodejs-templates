3a7d969e085339a30bf856700eebdee3a7fa4e75
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-14 13:27:06 +0100

    includes html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,26 +1,30 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
+const __DIRECTORYPATH = path.join(__dirname,'../public'); 
+app.use(express.static(__DIRECTORYPATH));
 
 //Making domain app.com
-app.get('/',(req,res) => {
-     res.send(`<h1>Hello world<h1>`);
+// app.get('/',(req,res) => {
+//      res.send(`<h1>Hello world<h1>`);
 
-});
+// });
 
-app.get('/help',(req,res) => {
-    res.send({
+// app.get('/help',(req,res) => {
+//     res.send({
 
     
-        name:'tijani',
-        age:33
-    });
+//         name:'tijani',
+//         age:33
+//     });
 
-})
+// })
 
-app.get('/about',(req,res) => {
-    res.send(`<h1>Hello world from about page <h1>`);
+// app.get('/about',(req,res) => {
+//     res.send(`<h1>Hello world from about page <h1>`);
 
-})
+// })
 
 
 app.get('/weather',(req,res) => {
@@ -31,7 +35,8 @@ app.get('/weather',(req,res) => {
         }
     );
 
-})
+});
+
 app.listen(3000,() => {
 
      console.log('Server Start on port 3000');
