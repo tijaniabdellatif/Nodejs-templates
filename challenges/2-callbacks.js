d4b09c46cd8f4f4d19b916aaa48e90dbff3b067c
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-14 02:40:35 +0100

    implementing callbacks

--- challenges/2-callbacks.js
+++ challenges/2-callbacks.js
@@ -9,3 +9,31 @@ const shortNames = names.filter(single => {
 
     return single.length <= 4;
 })
+
+const geoCode = (address,callback) => {
+
+    // const data = {
+
+    //     lat:'4334',
+    //     long:'56.5'
+    // }
+
+    // return data;
+
+    setTimeout(() => {
+        const data = {
+            lat:'4334',
+            long:'56.5'
+        }
+        // return data;
+        callback(data);
+    },2000)
+
+}
+
+// const data = geoCode('Rabat');
+// console.log(data);
+
+geoCode('Rabat',(data) => {
+    console.log(data);
+});
