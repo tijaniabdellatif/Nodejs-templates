164950566234aa5a836fd8f5e68f5ce21c8075f3
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-14 02:44:42 +0100

    finishing challenge callback

--- challenges/2-callbacks.js
+++ challenges/2-callbacks.js
@@ -1,39 +1,54 @@
-setTimeout(() => {
+// setTimeout(() => {
 
-    console.log('Two seconds are up')
+//     console.log('Two seconds are up')
 
-},2000);
+// },2000);
 
-const names = ['Tijani','Ami','Karim'];
-const shortNames = names.filter(single => {
+// const names = ['Tijani','Ami','Karim'];
+// const shortNames = names.filter(single => {
 
-    return single.length <= 4;
-})
+//     return single.length <= 4;
+// })
 
-const geoCode = (address,callback) => {
+// const geoCode = (address,callback) => {
 
-    // const data = {
+//     // const data = {
 
-    //     lat:'4334',
-    //     long:'56.5'
-    // }
+//     //     lat:'4334',
+//     //     long:'56.5'
+//     // }
 
-    // return data;
+//     // return data;
 
-    setTimeout(() => {
-        const data = {
-            lat:'4334',
-            long:'56.5'
-        }
-        // return data;
-        callback(data);
-    },2000)
+//     setTimeout(() => {
+//         const data = {
+//             lat:'4334',
+//             long:'56.5'
+//         }
+//         // return data;
+//         callback(data);
+//     },2000)
 
+// }
+
+// // const data = geoCode('Rabat');
+// // console.log(data);
+
+// geoCode('Rabat',(data) => {
+//     console.log(data);
+// });
+
+const add = (x,y,callback) => {
+
+    console.log('The sum is calculating');
+     setTimeout(() => {
+
+
+          callback(x + y);
+     },2000)
 }
 
-// const data = geoCode('Rabat');
-// console.log(data);
+add(1,4,(sum) => {
 
-geoCode('Rabat',(data) => {
-    console.log(data);
+    console.log(sum);
 });
