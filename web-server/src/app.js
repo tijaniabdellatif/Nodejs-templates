f6f4c43beaf905272b876695739a379fa8f53091
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-14 13:08:51 +0100

    send html and Json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,2 +1,38 @@
 const express = require('express');
+const app = express();
 
+//Making domain app.com
+app.get('/',(req,res) => {
+     res.send(`<h1>Hello world<h1>`);
+
+});
+
+app.get('/help',(req,res) => {
+    res.send({
+
+    
+        name:'tijani',
+        age:33
+    });
+
+})
+
+app.get('/about',(req,res) => {
+    res.send(`<h1>Hello world from about page <h1>`);
+
+})
+
+
+app.get('/weather',(req,res) => {
+    res.send(
+        {
+            forecast:'it s raining',
+            localion:'Rabat'
+        }
+    );
+
+})
+app.listen(3000,() => {
+
+     console.log('Server Start on port 3000');
+});
