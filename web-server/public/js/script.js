219687716c5e131d8a3e046fcd2b4a717337b4a6
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-03-15 18:59:34 +0100

    finishing the ui

--- web-server/public/js/script.js
+++ web-server/public/js/script.js
@@ -1,8 +1,8 @@
 const endpoint = 'https://puzzle.mead.io/puzzle';
 const forcast = 'http://127.0.0.1:3000/weather?address=Boston';
 const news = 'http://127.0.0.1:3000/articles';
-const morroco = 'http://127.0.0.1:3000/feeds?country=ma';
-
+const morroco = 'http://127.0.0.1:3000/feeds?country=';
+const form = document.querySelector('#form');
 
 const fetchData = async (uri) => {
 
@@ -22,69 +22,35 @@ const fetchData = async (uri) => {
 
 
 
+form.addEventListener('submit',(e) => {
+ 
+    e.preventDefault();
+    
+    const inputvalue = form.location.value;
 
-fetchData(news).then(data => {
-    if(data.error){
+    if(inputvalue === ''){
 
-        console.log(data.error);
+        alert('Fill the inout');
     }
     else {
+        fetchData(morroco+inputvalue).then(data => {
 
-      const {allnews} = data;
-      const parent = document.querySelector('.like');
-
-      const rawData = allnews.news.filter(element => {
-
-        return element.image_url !== null;
-      })
     
-      console.log(rawData);
-      
-         rawData.forEach(element => {
-
-            const card = document.createElement('div');
-            card.classList.add('like-card');
-            const {image_url,description,link,category} = element;
-    
-                let html = `
-                <img src="${image_url}" />
-                <p class="desc">${description.slice(0,60)}</p>
-                <p class="tags"><a href="${link}">${category[0]}</a></p>
-                `;
-
-                card.innerHTML = html;
-                parent.appendChild(card);
-               
-         })
-
+            if(data.error){
         
-
+               console.log(error);
+            }
         
-    }
-
-})
-
-fetchData(morroco).then(data => {
-
-    
-    if(data.error){
-
-       console.log(error);
-    }
-
-    else {
-
-        const {news:{results}} = data;
-        console.log(results)
-        const container = document.querySelector('.card-containers');
-
-        results.forEach(elem => {
-
-            const card = document.createElement('div');
-                    card.classList.add('card');
+            else {
+        
+                const {news:{results}} = data;
+                
+        
+                results.forEach(elem => {
                     const {name,description,url} = elem
-                    let html = `
-                    <div class="card-img">
+                    const card = document.querySelector('.card');
+
+                    card.innerHTML = `<div class="card-img">
                     <h5 class="title-card">${name}</h5>
                     <img src="" />
                </div>
@@ -92,50 +58,109 @@ fetchData(morroco).then(data => {
                <div class="card-container">
                    <p class="card-description">${description}</p>
                    <p class="card-anchor"><a href="${url}" target="_blank">view more ...</a></p>
-               </div>
-                    `;
-            
-                    card.innerHTML = html;
-                    container.appendChild(card);
+               </div>`;
+                  
+                })
+            }
+        
         })
         
-//        results.forEach(element => {
-//         const card = document.createElement('div');
-//         card.classList.add('card');
-//         const {name,description,url} = element
-//         let html = `
-//         <div class="card-img">
-//         <h5 class="title-card">${name}</h5>
-//         <img src="${image_url}" />
-//    </div>
-  
-//    <div class="card-container">
-//        <p class="card-description">${description}</p>
-//        <p class="card-anchor"><a href="${url}" target="_blank">view more ...</a></p>
-//    </div>
-        
-        
-//         `;
-
-//         card.innerHTML = html;
-//         container.appendChild(card);
-          
-//        })
 
     }
-
+    
 })
 
 
-fetchData(forcast).then(data => {
+window.addEventListener('DOMContentLoaded',() => {
+    fetchData(morroco+'ma').then(data => {
 
-    if(data.error){
+    
+        if(data.error){
+    
+           console.log(error);
+        }
+    
+        else {
+    
+            const {news:{results}} = data;
+        
+            const container = document.querySelector('.card-containers');
+    
+            results.forEach(elem => {
+    
+                const card = document.createElement('div');
+                        card.classList.add('card');
+                        const {name,description,url} = elem
+                        let html = `
+                        <div class="card-img">
+                        <h5 class="title-card">${name}</h5>
+                        <img src="" />
+                   </div>
+                  
+                   <div class="card-container">
+                       <p class="card-description">${description}</p>
+                       <p class="card-anchor"><a href="${url}" target="_blank">view more ...</a></p>
+                   </div>
+                        `;
+                
+                        card.innerHTML = html;
+                        container.appendChild(card);
+            })
+        }
+    
+    })
+    
+    fetchData(news).then(data => {
+        if(data.error){
+    
+            console.log(data.error);
+        }
+        else {
+    
+          const {allnews} = data;
+          const parent = document.querySelector('.like');
+    
+          const rawData = allnews.news.filter(element => {
+    
+            return element.image_url !== null;
+          })
+        
+          console.log(rawData);
+          
+             rawData.forEach(element => {
+    
+                const card = document.createElement('div');
+                card.classList.add('like-card');
+                const {image_url,description,link,category} = element;
+        
+                    let html = `
+                    <img src="${image_url}" />
+                    <p class="desc">${description.slice(0,60)}</p>
+                    <p class="tags"><a href="${link}">${category[0]}</a></p>
+                    `;
+    
+                    card.innerHTML = html;
+                    parent.appendChild(card);
+                   
+             })
+    
+            
+    
+            
+        }
+    
+    })
 
-        console.log(data.error);
-    }
-    else {
-
-        console.log(data.location);
-        console.log(data.forcast);
-    }
+    fetchData(forcast).then(data => {
+    
+        if(data.error){
+    
+            console.log(data.error);
+        }
+        else {
+    
+            console.log(data.location);
+            console.log(data.forcast);
+        }
+    })
 })
